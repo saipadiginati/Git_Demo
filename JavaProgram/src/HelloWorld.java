ba39ece36d0c066f845f78275592e67f087ed9e7
schaitanya17f11a0334-alt <schaitanya17f11a0334@gmail.com>
2026-07-20 12:09:12 +0530

    updated the code from Team member account (#4)
    Co-authored-by: saipadiginati <101805033+saipadiginati@users.noreply.github.com>

--- JavaProgram/src/HelloWorld.java
+++ JavaProgram/src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("hello World, Today is 20/07 from TL");
+        System.out.println("hello World, Today is 20/07 from Team Member");
     }
 }
